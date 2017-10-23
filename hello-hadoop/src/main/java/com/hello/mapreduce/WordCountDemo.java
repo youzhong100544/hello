2a7f99e7c1ebd56b5a0fe6760f44a34269d8bd6e
100200 <youzhong100544@aliyun.com>
2017-10-23 19:55:46 +0800

    update

---
+++ hello-hadoop/src/main/java/com/hello/mapreduce/WordCountDemo.java
@@ -0,0 +1,21 @@
+package com.hello.mapreduce;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
+public class WordCountDemo {
+	
+	public static void main(String[] args) throws IOException {
+		
+		Configuration conf = new Configuration(true);
+		
+		Job job = Job.getInstance(conf);
+		
+		
+		
+		
+	}
+}
