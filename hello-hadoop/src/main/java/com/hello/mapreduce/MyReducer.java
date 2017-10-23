b5599f7fa12c6c6449aa88e533b02c478315c0fb
100200 <youzhong100544@aliyun.com>
2017-10-23 20:33:26 +0800

    update

---
+++ hello-hadoop/src/main/java/com/hello/mapreduce/MyReducer.java
@@ -0,0 +1,23 @@
+package com.hello.mapreduce;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	
+	private IntWritable result = new IntWritable();
+	 
+	public void reduce(Key key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		int sum = 0;
+		for (IntWritable val : values) {
+			sum += val.get();
+		}
+		result.set(sum);
+		context.write(key, result);
+	}
+
+
+}
--- hello-hadoop/src/main/java/com/hello/mapreduce/WordCountDemo.java
+++ hello-hadoop/src/main/java/com/hello/mapreduce/WordCountDemo.java
@@ -1,21 +1,42 @@
 package com.hello.mapreduce;
 
+import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Job;
 
-import java.io.IOException;
-
 public class WordCountDemo {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		/*
+		// Create a new Job
+		Job job = Job.getInstance();
+		job.setJarByClass(MyJob.class);
+		
+		// Specify various job-specific parameters
+		job.setJobName("myjob");
+		
+		job.setInputPath(new Path("in"));
+		job.setOutputPath(new Path("out"));
+		
+		job.setMapperClass(MyJob.MyMapper.class);
+		job.setReducerClass(MyJob.MyReducer.class);
+		
+		// Submit the job, then poll for progress until the job is complete
+		job.waitForCompletion(true);
+		*/
+		
 		
 		Configuration conf = new Configuration(true);
-		
 		Job job = Job.getInstance(conf);
 		
+		job.setJarByClass(WordCountDemo.class);
 		
 		
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+		
+		job.waitForCompletion(true);
 		
 	}
 }
