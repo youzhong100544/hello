9dbd08b982b9173fceeececfd864983748b4db9e
100200 <youzhong100544@aliyun.com>
2017-10-26 15:13:14 +0800

    init

--- hello-hadoop/src/main/java/com/hello/mapreduce/demo/tq/MyTQ.java
+++ hello-hadoop/src/main/java/com/hello/mapreduce/demo/tq/MyTQ.java
@@ -1,9 +1,16 @@
 package com.hello.mapreduce.demo.tq;
 
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
 public class MyTQ {
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws IOException {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+		
 	}
 }
