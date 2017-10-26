9d6edc2cf324b36526f1c6c59808b4bafe6e9e45
dafochaodong <youzhong100544@aliyun.com>
2017-10-26 22:45:13 +0800

    init

--- hello-hadoop/src/main/java/com/hello/mapreduce/demo/tq/TQReducer.java
+++ hello-hadoop/src/main/java/com/hello/mapreduce/demo/tq/TQReducer.java
@@ -11,11 +11,37 @@ public class TQReducer extends Reducer<TQ, IntWritable, Text, IntWritable>{
 	@Override
 	protected void reduce(TQ key, Iterable<IntWritable> values, Reducer<TQ, IntWritable, Text, IntWritable>.Context context)
 			throws IOException, InterruptedException {
+
+
+		Text rkey = new Text();
+		IntWritable rval = new IntWritable();
 		// 年月相同为一组
 		// 温度倒序
-		
-		for (IntWritable intWritable : values) {
-			
+		int f = 0;
+
+		int day = 0;
+		for (IntWritable v : values) {
+
+			if ( f == 0) {
+
+				rkey.set(key.getYear()+ "-" + key.getMonth() + "-" +key.getDay());
+				rval.set(key.getTemperature());
+
+				context.write(rkey,rval);
+
+				day = key.getDay();
+				f++;
+			}
+
+			if ( f != 0 && day != key.getDay() ) {
+
+				rkey.set(key.getYear()+ "-" + key.getMonth() + "-" +key.getDay());
+				rval.set(key.getTemperature());
+
+				context.write(rkey,rval);
+				break;
+			}
+
 		}
 		
 	}
