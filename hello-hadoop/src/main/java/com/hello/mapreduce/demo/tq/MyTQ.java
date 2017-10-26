0ded111a956e5ad3fd2f576ac0c620b070572627
100200 <youzhong100544@aliyun.com>
2017-10-26 15:18:29 +0800

    commit tq

--- hello-hadoop/src/main/java/com/hello/mapreduce/demo/tq/MyTQ.java
+++ hello-hadoop/src/main/java/com/hello/mapreduce/demo/tq/MyTQ.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class MyTQ {
 	
 	public static void main(String[] args) throws IOException {
+		
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf);
 		
