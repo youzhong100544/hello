a787b1639ccf20869dfafe63965fbedac46f2b95
100200 <youzhong100544@aliyun.com>
2017-10-27 09:47:07 +0800

    update test

--- hello-hadoop/src/test/java/com/hello/test/HadoopTest.java
+++ hello-hadoop/src/test/java/com/hello/test/HadoopTest.java
@@ -14,19 +14,19 @@ public class HadoopTest {
 //		System.err.println(localDate.toString());
 		
 		DateTimeFormatter basicIsoDate = DateTimeFormatter.BASIC_ISO_DATE;
-		System.err.println(basicIsoDate);
+		System.err.println(basicIsoDate);//ParseCaseSensitive(false)Value(Year,4)Value(MonthOfYear,2)Value(DayOfMonth,2)[Offset(+HHMMss,'Z')]
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		
 		LocalDateTime localDateTime = LocalDateTime.parse("1949-10-01 14:21:02",format);
-		System.err.println("localDateTime" + localDateTime.toString());
+		System.err.println("localDateTime" + localDateTime.toString());//localDateTime1949-10-01T14:21:02
 		
-		System.err.println("year: " + localDateTime.getYear());
-		System.err.println("month: " + localDateTime.getMonthValue());
-		System.err.println("month: " + localDateTime.getMonth().toString());
+		System.err.println("year: " + localDateTime.getYear());//year: 1949
+		System.err.println("month: " + localDateTime.getMonthValue());//month: 10
+		System.err.println("month: " + localDateTime.getMonth().toString());//month: OCTOBER
 		
-		System.err.println("day: " + localDateTime.getDayOfMonth());
-		System.err.println("day: " + localDateTime.getDayOfYear());
-		System.err.println("day: " + localDateTime.getDayOfWeek().toString());
+		System.err.println("day: " + localDateTime.getDayOfMonth());//day: 1
+		System.err.println("day: " + localDateTime.getDayOfYear());//day: 274
+		System.err.println("day: " + localDateTime.getDayOfWeek().toString());//day: SATURDAY
 		
 		
 	}
