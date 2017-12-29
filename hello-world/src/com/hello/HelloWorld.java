0bf2309231a7f8ca05a64d42190022090cbec33d
100200 <youzhong100544@aliyun.com>
2017-12-29 09:13:31 +0800

    commit

--- hello-world/src/com/hello/HelloWorld.java
+++ hello-world/src/com/hello/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello C");
+		System.out.println("Hello java");
+		System.out.println("Hello hadoop");
+		System.out.println("World is World");
 	}
 
 }
