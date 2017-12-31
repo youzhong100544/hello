15ab4446c71d543c74ffc8730c3dc7316bd70235
youzhong100544 <youzhong100200@126.com>
2017-12-31 19:56:47 +0800

    init spark

---
+++ hello-spark/src/com/hello/spark/java/sparksql/dataframe/SparkSQLDemo.java
@@ -0,0 +1,133 @@
+package com.hello.spark.java.sparksql.dataframe;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
+
+import com.hello.spark.java.bean.Student;
+
+
+public class SparkSQLDemo {
+
+	public static void main(String[] args) {
+		SparkConf sparkConf = new SparkConf();
+		sparkConf.setMaster("local").setAppName("SparkSQLDemo");
+		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+		
+		SQLContext sqlContext = new SQLContext(sparkContext);
+		
+		/**
+		 * 读取普通格式文件
+		 * 把普通格式的RDD创建DataFrame
+		 * 
+		 * 1,zhangsan,18
+		 * 2,lisi,19
+		 * 3,wangwu,20
+		 */
+		JavaRDD<String> lineRDD = sparkContext.textFile("student");
+
+		JavaRDD<Student> studentRDD = lineRDD.map(new Function<String, Student>() {
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Student call(String line) throws Exception {
+
+				Student student = new Student();
+				
+				student.setId(line.split(",")[0]);
+				student.setName(line.split(",")[1]);
+				student.setAge(Integer.valueOf(line.split(",")[2]));
+				
+				return student;
+			}
+		});
+		
+		/**
+		 * 传入进去Student.class的时候，sqlContext是通过反射的方式创建DataFrame
+		 * 在底层通过反射的方式获得Student的所有field，结合RDD本身，就生成了DataFrame
+		 */
+		DataFrame dataFrame = sqlContext.createDataFrame(studentRDD, Student.class);
+		dataFrame.show();
+		dataFrame.printSchema();
+		/**
+		 *
+		 	+---+---+--------+
+			|age| id|    name|
+			+---+---+--------+
+			| 18|  1|zhangsan|
+			| 19|  2|    lisi|
+			| 20|  3|  wangwu|
+			+---+---+--------+
+			
+			root
+			 |-- age: integer (nullable = true)
+			 |-- id: string (nullable = true)
+			 |-- name: string (nullable = true)
+		 *
+		 * */
+		
+		dataFrame.registerTempTable("student_table");
+		// TODO
+		System.out.println("1-------------------------------------");
+		
+		
+		DataFrame selectResult = sqlContext.sql("select * from student_table where id = '1'");
+		
+		selectResult.show();
+		/**
+		 * 
+			+---+---+--------+
+			|age| id|    name|
+			+---+---+--------+
+			| 18|  1|zhangsan|
+			+---+---+--------+
+		 * 
+		 */
+		// TODO
+		System.out.println("2-------------------------------------");
+		
+		
+		selectResult = sqlContext.sql("select id, name, age from student_table where name = 'lisi'");
+		
+		selectResult.show();
+		/**
+		 * 
+		 	+---+----+---+
+			| id|name|age|
+			+---+----+---+
+			|  2|lisi| 19|
+			+---+----+---+	
+		  
+		 *  
+		 * */
+		// TODO
+		System.out.println("3-------------------------------------");
+		
+		selectResult = sqlContext.sql("select name, age from student_table where age > 18");
+		
+		selectResult.show();
+		/**
+		 *
+		 	+------+---+
+			|  name|age|
+			+------+---+
+			|  lisi| 19|
+			|wangwu| 20|
+			+------+---+
+		 *  
+		 * */
+		// TODO
+		System.out.println("4-------------------------------------");
+		
+		
+		// 关闭资源
+		sparkContext.close();
+	}
+}
---
+++ hello-spark/src/com/hello/spark/scala/sparksql/dataframe/SparkSQLDemo.scala
@@ -0,0 +1,46 @@
+package com.hello.spark.scala.sparksql.dataframe
+
+import org.apache.spark.sql.{DataFrameReader, SQLContext}
+import org.apache.spark.{SparkConf, SparkContext}
+import com.hello.spark.scala.bean.Student
+
+object SparkSQLDemo {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf()
+    
+    sparkConf.setMaster("local").setAppName("SparkSQLDemo")
+   
+		val sparkContext = new SparkContext(sparkConf)
+    
+    val SQLContext = new SQLContext(sparkContext)
+    
+    val lineRDD = sparkContext.textFile("student")
+    
+    /**
+     * 将RDD隐式转换成DataFrame
+     */
+    //import sqlContext.implicits._
+    
+    // 把普通格式的RDD创建DataFrame
+    val studentRDD  = lineRDD.map(line => {
+      val student = Student(line.split(",")(0), line.split(",")(1), Integer.valueOf(line.split(",")(2)))
+      student
+    })
+    
+    //导入隐式转换，如果不导入无法将RDD转换成DataFrame
+    //将RDD转换成DataFrame
+    import sqlContext.implicits._
+    val studentDataFrame = studentRDD.toDF();
+
+    //注册表
+    studentDataFrame.registerTempTable("student_table")
+    //传入SQL
+    val df = sqlContext.sql("select * from student_table order by age desc limit 2")
+    
+    
+    // 关闭资源
+    sparkContext.stop()
+    
+  }
+}
+
