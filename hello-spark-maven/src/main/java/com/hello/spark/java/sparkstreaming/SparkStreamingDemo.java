d784b95d592f904965464472051b33f69ad64dfe
youzhong100544 <youzhong100200@126.com>
2018-01-02 21:29:04 +0800

    init spark-maven

--- hello-spark-maven/src/main/java/com/hello/spark/java/sparkstreaming/SparkStreamingDemo.java
+++ hello-spark-maven/src/main/java/com/hello/spark/java/sparkstreaming/SparkStreamingDemo.java
@@ -2,8 +2,18 @@ package com.hello.spark.java.sparkstreaming;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 
 public class SparkStreamingDemo {
@@ -20,14 +30,56 @@ public class SparkStreamingDemo {
 
 		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
 
-		// 方式一：sparkcontext
-		JavaStreamingContext javaStreamingContext = new JavaStreamingContext(sparkContext, Durations.seconds(5));
-		
-		// 方式二：sparkconf
-		JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(5));
-		
-		
-		
+		// 方式一：sparkconf
+		JavaStreamingContext javaStreamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(5));
+
+		// 方式二：sparkcontext
+		JavaStreamingContext streamingContext = new JavaStreamingContext(sparkContext, Durations.seconds(5));
+
+		JavaReceiverInputDStream<String> lines = streamingContext.socketTextStream("node5", 9999);
+
+
+		JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+			/**
+			 *
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Iterator<String> call(String s) {
+				return Arrays.asList(s.split(" ")).iterator();
+			}
+		});
+
+		JavaPairDStream<String,Integer> pair = words.mapToPair(new PairFunction<String, String, Integer>() {
+
+			/**
+			 *
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Tuple2<String, Integer> call(String word) throws Exception {
+				return new Tuple2<String, Integer>(word,1);
+			}
+		});
+
+		JavaPairDStream<String,Integer> result = pair.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+			/**
+			 *
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Integer call(Integer i1, Integer i2) throws Exception {
+				return i1 + i2;
+			}
+		});
+
+		//outputoperator类的算子
+		result.print();
+
 		/**
 		 * 5、JavaStreamingContext.start()straming框架启动之后是不能在次添加业务逻辑
 		 * 6、JavaStreamingContext.stop()无参的stop方法会将sparkContext一同关闭，stop(false) ,默认为true，会一同关闭
