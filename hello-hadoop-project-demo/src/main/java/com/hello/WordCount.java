571d50345d2e7b3b7a24d37649b33f60212f401b
youzhong100544 <youzhong100200@126.com>
2018-01-10 21:35:38 +0800

    commit hadoop project

--- hello-hadoop-project-demo/src/main/java/com/hello/WordCount.java
+++ hello-hadoop-project-demo/src/main/java/com/hello/WordCount.java
@@ -1,6 +1,11 @@
-package com.hello.mapreduce;
+package com.hello;
 
+import com.hello.common.GlobalConstants;
+import com.hello.util.TimeUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -22,19 +27,55 @@ public class WordCount implements Tool {
 
 	@Override
 	public int run(String[] args) throws Exception {
+		Configuration conf = this.getConf();
+		this.processArgs(conf, args);
+
+		Job job = Job.getInstance(conf, "analyser_logdata");
+
 		return 0;
 	}
 
 	@Override
 	public void setConf(Configuration conf) {
-		conf = HBaseConfiguration.create();
 
-		// 如果是伪分布式  zk指定hbase
 		conf.set("hbase.zookeeper.quorum", "node2,node3,node4");
+		conf.set("fs.defaultFS", "hdfs://node1:8020");
+//		conf.set("yarn.resourcemanager.hostname", "node3");
+
+		this.conf = HBaseConfiguration.create(conf);
 	}
 
 	@Override
 	public Configuration getConf() {
-		return null;
+		return this.conf;
+	}
+
+
+	/**
+	 * 处理参数
+	 *
+	 * @param conf
+	 * @param args
+	 */
+	private void processArgs(Configuration conf, String[] args) {
+		String date = null;
+		for (int i = 0; i < args.length; i++) {
+			if ("-d".equals(args[i])) {
+				if (i + 1 < args.length) {
+					date = args[++i];
+					break;
+				}
+			}
+		}
+
+		System.out.println("-----" + date);
+
+		// 要求date格式为: yyyy-MM-dd
+		if (StringUtils.isBlank(date) || !TimeUtil.isValidateRunningDate(date)) {
+			// date是一个无效时间数据
+			date = TimeUtil.getYesterday(); // 默认时间是昨天
+			System.out.println(date);
+		}
+		conf.set(GlobalConstants.RUNNING_DATE_PARAMES, date);
 	}
 }
