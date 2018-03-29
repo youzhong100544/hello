920de6390fff58fb03080fdd67605df7d95f2a04
100200 <youzhong100544@aliyun.com>
2018-03-29 16:01:46 +0800

    delete

--- spark-project-demo-sbt/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "spark-project-demo-sbt"
-
-version := "1.0"
-
-scalaVersion := "2.12.1"
-        
--- spark-project-demo-sbt/src/test/scala/com/spark/project/test/TestDemoScala.scala
+++
@@ -1,8 +0,0 @@
-package com.spark.project.test
-
-/**
-  * Created by fanlinglong on 2018/1/23.
-  */
-object TestDemoScala {
-
-}
