f147997fab409c419553b696191401491c67e3b4
100200 <youzhong100544@aliyun.com>
2018-03-29 17:05:02 +0800

    init

---
+++ project-spark-demo/src/com/spark/project/java/WordCountJava.java
@@ -0,0 +1,7 @@
+package com.spark.project.java;
+
+/**
+ * Created by fanlinglong on 2018/3/29.
+ */
+public class WordCountJava {
+}
---
+++ project-spark-demo/src/com/spark/project/scala/WordCountScala.scala
@@ -0,0 +1,8 @@
+package com.spark.project.scala
+
+/**
+  * Created by fanlinglong on 2018/3/29.
+  */
+object WordCountScala {
+
+}
