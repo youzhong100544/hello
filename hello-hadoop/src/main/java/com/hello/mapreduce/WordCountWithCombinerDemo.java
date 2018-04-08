16f895dd6b82bcf1d3a398e0334587d01eb8b7ba
calm <youzhong100544@126.com>
2018-04-08 22:24:33 +0800

    init

--- hello-hadoop/src/main/java/com/hello/mapreduce/WordCountWithCombinerDemo.java
+++ hello-hadoop/src/main/java/com/hello/mapreduce/WordCountWithCombinerDemo.java
@@ -1,22 +1,75 @@
 package com.hello.mapreduce;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 
 public class WordCountWithCombinerDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(WordCountWithCombinerDemo.class);
 		
+		// 输入
+		Path input = new Path("/user/root/wc.txt");
+		FileInputFormat.addInputPath(job, input);
+		
+		// 输出
+		Path output = new Path("/output");
+		if (output.getFileSystem(conf).exists(output)) {
+			output.getFileSystem(conf).delete(output,true);
+		}
+		FileOutputFormat.setOutputPath(job, output);
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setCombinerClass(WordCountReducer.class);
+		job.setReducerClass(WordCountReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 		
 	}
 	
 	public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable>{
-		
+			   
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		 
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
 	}
 	
 	public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 		
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+	   }
+		
 	}
+
 }
