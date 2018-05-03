6a5bb505c17598902ea247e3d81bf6cab12a08b8
calm <youzhong100544@126.com>
2018-05-03 17:48:49 +0800

    init

--- hello-spark-java-maven/src/main/java/com/hello/spark/java/WordCountHDFS.java
+++ hello-spark-java-maven/src/main/java/com/hello/spark/java/WordCountHDFS.java
@@ -1,9 +1,8 @@
 package com.hello.spark.java;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -12,33 +11,51 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.api.java.function.VoidFunction;
-
 import scala.Tuple2;
 
-public class WordCount {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-	public static void main(String[] args) {
+public class WordCountHDFS {
+
+	public static void main(String[] args) throws IOException {
 		SparkConf sparkConf = new SparkConf();
 
-		sparkConf.setMaster("local").setAppName("WordCount");
+		sparkConf.setMaster("local").setAppName("WordCountHDFS");
 
 		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
 
+		Configuration hadoopConfiguration = sparkContext.hadoopConfiguration();
+		FileSystem fileSystem = FileSystem.get(hadoopConfiguration);
 
-		JavaRDD<String> line = sparkContext.textFile("C:\\Users\\dafochaodong\\Desktop\\wc.txt");
+		String hdfsInputPath = "hdfs://node:9000/user/root/wc.txt";
+		String hdfsOutputPath = "hdfs://node:9000/output/spark/wordcount";
 
+		JavaRDD<String> line = sparkContext.textFile(hdfsInputPath);
+		FsStatus status = fileSystem.getStatus(new Path(hdfsInputPath));
+		System.out.println(status.toString());
+		FileStatus fileStatus = fileSystem.getFileStatus(new Path(hdfsInputPath));
+		BlockLocation[] fileBlockLocations = fileSystem.getFileBlockLocations(fileStatus, 0, fileStatus.getLen());
+		for (int i = 0; i < fileBlockLocations.length; i++) {
+			System.out.println(fileBlockLocations[i]);
+		}
 
-		JavaRDD<String> words = line.flatMap(new FlatMapFunction<String, String>() {
+		JavaRDD<String> words = line.flatMap(new FlatMapFunction<String,String>(){
+
 			/**
-			 *
+			 * 
 			 */
 			private static final long serialVersionUID = 1L;
 
-			@Override
-			public Iterator<String> call(String line) {
+			public Iterator<String> call(String line) throws Exception {
+
 				return Arrays.asList(line.split(" ")).iterator();
 			}
+
 		});
+
 
 		JavaPairRDD<String,Integer> pair = words.mapToPair(new PairFunction<String, String, Integer>() {
 
@@ -48,7 +65,6 @@ public class WordCount {
 			private static final long serialVersionUID = 1L;
 
 
-			@Override
 			public Tuple2<String, Integer> call(String word) throws Exception {
 				return new Tuple2<String, Integer>(word, 1);
 			}
@@ -61,7 +77,6 @@ public class WordCount {
 			 */
 			private static final long serialVersionUID = 1L;
 
-			@Override
 			public Integer call(Integer i1, Integer i2) throws Exception {
 				return i1 + i2;
 			}
@@ -75,16 +90,19 @@ public class WordCount {
 			 */
 			private static final long serialVersionUID = 1L;
 
-			@Override
 			public void call(Tuple2<String, Integer> tuple) throws Exception {
 				System.out.println(tuple);
 			}
 		});
 		// 排序
 		// K V 倒置
-		JavaPairRDD<Integer,String> keyValueConvertResult = result.mapToPair(new PairFunction<Tuple2<String,Integer>, Integer, String>() {
+		JavaPairRDD<Integer, String> keyValueConvertResult = result.mapToPair(new PairFunction<Tuple2<String,Integer>, Integer, String>() {
 
-			@Override
+			/**
+			 *
+			 */
+			private static final long serialVersionUID = 1L;
+
 			public Tuple2<Integer, String> call(Tuple2<String, Integer> tuple) throws Exception {
 				return new Tuple2<Integer, String>(tuple._2, tuple._1);
 			}
@@ -100,7 +118,6 @@ public class WordCount {
 			 */
 			private static final long serialVersionUID = 1L;
 
-			@Override
 			public void call(Tuple2<Integer, String> tuple) throws Exception {
 
 				System.out.println(tuple._2 + "," + tuple._1);
@@ -112,6 +129,12 @@ public class WordCount {
 		for (Tuple2<Integer, String> tuple : take) {
 			System.out.println(tuple._2 + "," + tuple._1);;
 		}
+
+		if (fileSystem.exists(new Path(hdfsOutputPath))) {
+			fileSystem.delete(new Path(hdfsOutputPath),true);
+		}
+
+		resultSortByKey.saveAsTextFile(hdfsOutputPath);
 
 		sparkContext.close();
 	}
