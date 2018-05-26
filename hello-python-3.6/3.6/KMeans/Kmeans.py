c27a069a82f1c86000465dbc4f566145c683cd4d
calm <youzhong100544@126.com>
2018-05-26 11:14:56 +0800

    init

--- hello-python-3.6/3.6/KMeans/Kmeans.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from click._compat import raw_input
-
--- hello-python-3.6/3.6/demo-3.6.py
+++
@@ -1,7 +0,0 @@
-# demo-python 3.6
-# coding=utf-8
-# -*- coding: utf-8 -*-
-
-print "hello - python 3.6";
-print "你好，3.6";
-
