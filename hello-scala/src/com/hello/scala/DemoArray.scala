d7738f0c3d1e48fb4f3601540aaeedb40222c52a
work <youzhong100544@aliyun.com>
2018-06-14 19:31:52 +0800

    init

---
+++ hello-scala/src/com/hello/scala/DemoArray.scala
@@ -0,0 +1,72 @@
+package com.hello.scala
+
+object DemoArray {
+
+  def main(args: Array[String]): Unit = {
+
+    var z: Array[String] = new Array[String](3)
+
+    z(0) = "Runoob"; z(1) = "Baidu"; z(4/2) = "Google"
+
+    // 打印
+    println(z) //[Ljava.lang.String;@ea4a92b
+
+    // 循环遍历打印1
+    z.foreach(println)
+
+    // 循环遍历打印2
+    for (elem <- z) {
+      print(elem + ",")
+    }
+    println()
+    println()
+    for ( i <- 0 to (z.length - 1)) {
+      if (i != z.length- 1){
+        print(z(i) + ',')
+      }else{
+        print(z(i))
+      }
+    }
+    println()
+    println()
+    // 创建数组 ---------------------------------------------------
+
+    // 创建数组
+    var a = Array("Runoob", "Baidu", "Google")
+
+    // 多维数组----------------------------------------------------------
+    import Array._
+    var multidimensionalArray = ofDim[Int](2,3)
+
+    // 创建矩阵
+    for (i <- 0 to 1) {
+      for ( j <- 0 to 2) {
+        multidimensionalArray(i)(j) = j;
+      }
+    }
+    // 打印二维阵列
+    for (i <- 0 to 1) {
+      for ( j <- 0 to 2) {
+        print(" " + multidimensionalArray(i)(j));
+      }
+      println();
+    }
+    println();
+    println();
+    // 打印二维阵列
+    for (i <- 0 to 1 ; j <- 0 to 2) {
+        multidimensionalArray(i)(j) = j;
+    }
+    // 打印二维阵列
+    for (i <- 0 to 1 ; j <- 0 to 2) {
+      print(" " + multidimensionalArray(i)(j));
+      println();
+    }
+    println();
+    println();
+
+
+
+  }
+
+}
--- hello-scala/src/com/hello/scala/DemoScala.scala
+++ hello-scala/src/com/hello/scala/DemoScala.scala
@@ -8,6 +8,28 @@ object DemoScala {
     val add = testScala.addInt(1, 2)
     println("1 + 2 = " + add)
 
+    val ids = 1 to 10
+    println("ids " + ids)
+    println("type-ids " + ids.asInstanceOf[AnyRef].getClass.getSimpleName)
+    println("type-ids " + ids.asInstanceOf[AnyRef].getClass.getName)
+    // scala.collection.immutable.Range.Inclusive
+
+    val i = 1
+    println("i " + i)
+    println("i " + i.asInstanceOf[AnyRef].getClass.getSimpleName)
+    println("i " + i.asInstanceOf[AnyRef].getClass.getName)
+
+    val is = i.toString
+    println("is " + is)
+    println("is " + is.asInstanceOf[AnyRef].getClass.getSimpleName)
+    println("is " + is.asInstanceOf[AnyRef].getClass.getName)
+
+
+    var is_var = i.toString
+    println("is_var " + is_var)
+    println("is_var " + is_var.asInstanceOf[AnyRef].getClass.getSimpleName)
+
+
 
   }
 
