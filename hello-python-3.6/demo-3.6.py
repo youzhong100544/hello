7d3a74727c685962fc837c9ba17f69a2a9b6a774
work <youzhong100544@aliyun.com>
2018-06-14 14:55:34 +0800

    update

--- hello-python-3.6/demo-3.6.py
+++ hello-python-3.6/demo-3.6.py
@@ -10,13 +10,39 @@ print("你好，3.6")
 print('-------------------------------------------------------')
 
 def demo_print():
-    print ("demo_print")
+	print("demo_print")
 
 def demo_print_arg(str):
-    print ("demo_print_arg: " + str)
+	print("demo_print_arg: " + str)
+
+
+# 加法
+def demo_add(a, b):
+	return a, "+", b, a+b
+# 减法
+def demo_subtraction(a, b):
+	return a, '-', b, a-b
+# 乘法
+def demo_multiplication(a, b):
+	return a, '*', b, a*b
+# 除法
+def demo_division(a, b):
+	return a, '/', b, a/b
 
 
 if __name__ == '__main__':
-    demo_print()
-    demo_print_arg('arg')
+	demo_print()
+	demo_print_arg('arg')
+
+	add = demo_add(2, 5)
+	print(add)
+
+	subtraction = demo_subtraction(2, 5)
+	print(subtraction)
+
+	multiplication = demo_multiplication(2, 5)
+	print(multiplication)
+
+	division = demo_division(2, 5)
+	print(division)
 
--- hello-python-3.6/hello-python-3.6.py
+++ hello-python-3.6/hello-python-3.6.py
@@ -3,7 +3,20 @@
 
 # 注意在python2中print不是函数，不能通过help获得相关信息
 # print在python3中是一个内建函数
-print ("hello - python 3.6")
-print ('你好，3.6')
+print("hello - python 3.6")
+print('你好，3.6')
 
+
+
+# 导入内置math模块
+
+import sys as system
+
+print(system.platform)
+
+
+import math
+
+content = dir(math)
+print(content);
 
