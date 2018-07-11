3b3a48f94016f101c515130e3cc05563441cecf5
calm <youzhong100544@126.com>
2018-07-11 11:53:36 +0800

    init

--- hello-hadoop/src/main/java/com/hello/MapReduceDesignPatterns/utils/MRDPUtils.java
+++ hello-hadoop/src/main/java/com/hello/MapReduceDesignPatterns/utils/MRDPUtils.java
@@ -1,4 +1,27 @@
 package com.hello.MapReduceDesignPatterns.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MRDPUtils {
+
+	public static Map<String, String> transformXmlToMap(String xml) {
+		Map<String, String> map = new HashMap();
+
+		try {
+			String[] tokens = xml.trim().substring(5, xml.trim().length() - 3).split("\"");
+
+			for (int i = 0; i < tokens.length - 1; i += 2) {
+				String key = tokens[i].trim();
+				String value = tokens[i + 1];
+
+				map.put(key.substring(0, key.length() - 1), value);
+			}
+		}catch (StringIndexOutOfBoundsException e){
+			System.out.println(e);
+		}
+
+		return map;
+	}
+
 }
