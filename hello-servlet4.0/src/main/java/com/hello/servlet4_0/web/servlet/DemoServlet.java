d12836198d2c5355c417701f506c5bc0048d31f5
work <youzhong100544@aliyun.com>
2018-07-30 17:51:00 +0800

    init

--- hello-servlet4.0/src/main/java/com/hello/servlet4_0/web/servlet/DemoServlet.java
+++ hello-servlet4.0/src/main/java/com/hello/servlet4_0/web/servlet/DemoServlet.java
@@ -1,7 +1,6 @@
 package com.hello.servlet4_0.web.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
 
 }
