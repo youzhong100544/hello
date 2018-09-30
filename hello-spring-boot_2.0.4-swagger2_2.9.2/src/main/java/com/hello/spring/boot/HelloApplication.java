bf1e816c0d8a67420473a36b993824dca81feb57
work <youzhong100544@aliyun.com>
2018-09-30 11:12:39 +0800

    init

--- hello-spring-boot_2.0.4-swagger2_2.9.2/src/main/java/com/hello/spring/boot/HelloApplication.java
+++ hello-spring-boot_2.0.4-swagger2_2.9.2/src/main/java/com/hello/spring/boot/HelloApplication.java
@@ -1,7 +1,12 @@
 package com.hello.spring.boot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 /**
  * @SpringBootApplication注解呢是SpringBoot的一个组合注解，
@@ -11,5 +16,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	/**
+	 * springboot在启动的时候为我们注入了哪些bean
+	 *
+	 * @param applicationContext
+	 * @return
+	 */
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+		return args -> {
+			long startTime = 0l;
+			long endTime = 0l;
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			int beanDefinitionCount = applicationContext.getBeanDefinitionCount();
+			System.out.println("beanDefinitionCount:" + beanDefinitionCount);
+			String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+			Arrays.sort(beanDefinitionNames);
+			System.out.println("beanDefinitionName:");
+
+			startTime = System.currentTimeMillis();
+			for (int i = 0; i < beanDefinitionNames.length; i++) {
+				System.out.println(beanDefinitionNames[i]);
+			}
+			endTime = System.currentTimeMillis();
+			System.out.println("execution time:" + (endTime - startTime));
+		};
 	}
 }
