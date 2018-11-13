460aa5c880cd8e78ebfc79f3fa8b47f26fc5d9f2
youzhong100200 <youzhong100200@126.com>
2018-11-13 19:43:59 +0800

    init python开发spark

--- hello-spark2.3.0-python3.6/demo/spark_python_word_count_local.py
+++ hello-spark2.3.0-python3.6/demo/spark_python_word_count_local.py
@@ -1,6 +1,16 @@
 #coding=utf-8
 
-from pyspark import SparkContext, SparkConf
+import findspark
+findspark.init()
+
+from pyspark import SparkConf, SparkContext
+
+import os
+import sys
+os.environ['SPARK_HOME'] = "C:\\develop\\spark-2.3.0-bin-hadoop2.7"
+os.environ['JAVA_HOME'] = "C:\\develop\\Java\\jdk1.8.0_161"
+sys.path.append("C:\\develop\\spark-2.3.0-bin-hadoop2.7")
+os.environ['HADOOP_HOME'] = "C:\\develop\\hadoop-3.1.0"
 
 input_path = 'C://Users/calm/Desktop/hello.txt'
 output_path = 'C://Users/calm/Desktop/output'
--- hello-spark2.3.0-python3.6/demo/word_count_demo.py
+++ hello-spark2.3.0-python3.6/demo/word_count_demo.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
+
+import findspark
+findspark.init()
+
 from pyspark import SparkConf, SparkContext
 
+import os
+import sys
+os.environ['SPARK_HOME'] = "C:\\develop\\spark-2.3.0-bin-hadoop2.7"
+os.environ['JAVA_HOME'] = "C:\\develop\\Java\\jdk1.8.0_161"
+sys.path.append("C:\\develop\\spark-2.3.0-bin-hadoop2.7")
+os.environ['HADOOP_HOME'] = "C:\\develop\\hadoop-3.1.0"
+
+
 # 创建SparkConf和SparkContext
-conf = SparkConf().setMaster("local").setAppName(appName = "word_count_demo")
+conf = SparkConf().setMaster("local").setAppName("word_count_demo")
 sc = SparkContext(conf = conf)
 
 # 输入的数据
