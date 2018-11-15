a89910866d2da050c45b57798e4966448c66fe16
youzhong100200 <youzhong100200@126.com>
2018-11-15 14:37:42 +0800

    init spark

--- hello-spark2.3.1-scala2.11.12-maven/src/main/scala/com/hello/spark/scala/util/CommomUtil.scala
+++ hello-spark2.3.1-scala2.11.12-maven/src/main/scala/com/hello/spark/scala/util/CommomUtil.scala
@@ -3,11 +3,18 @@ package com.hello.spark.scala.util
 import java.io.File
 
 object CommomUtil {
+
+  def deleteDir(dirPath: String): Boolean = {
+    deleteDir(new File(dirPath))
+  }
   /**
     * 删除一个文件夹,及其子目录
+    *
     * @param dir
+    *            将要删除的文件目录
+    * @return
     */
-  def deleteDir(dir: File): Unit = {
+  def deleteDir(dir: File): Boolean = {
     val files = dir.listFiles()
     files.foreach(f => {
       if (f.isDirectory) {
