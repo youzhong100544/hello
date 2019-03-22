e520e4ba91d64131e99d93f51de614938177f6bb
youzhong100200 <youzhong100200@126.com>
2019-03-22 13:44:46 +0800

    update

--- hello-spark2.3.1-scala2.11.12-maven/src/main/scala/com/hello/spark/scala/core/operator/action/ForeachPartition.scala
+++ hello-spark2.3.1-scala2.11.12-maven/src/main/scala/com/hello/spark/scala/core/operator/action/ForeachPartition.scala
@@ -3,6 +3,8 @@ package com.hello.spark.scala.core.operator.action
 import org.apache.spark.{SparkConf, SparkContext, TaskContext}
 import org.apache.spark.rdd.RDD
 
+import scala.collection.immutable.List
+
 object ForeachPartition {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Foreach").setMaster("local")
@@ -15,9 +17,14 @@ object ForeachPartition {
     rdd.foreachPartition((word :Iterator[String])=>{
       println("当前partitionId：" + TaskContext.get.partitionId)
       word.foreach(println)
+    })
 
+    println("++++++++++++++++++++++++++++++++++++++++++++")
 
-
+    rdd.foreachPartition((word :Iterator[String])=>{
+      var map = scala.collection.mutable.Map[Int,List[String]]()
+      map += (TaskContext.get.partitionId -> word.toList)
+      map.iterator.foreach(println)
     })
   }
 }
