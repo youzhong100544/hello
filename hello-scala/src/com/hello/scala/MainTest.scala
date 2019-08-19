cdcddb2fa19ee12a9b4e891e489259abe8b443c5
姜海涛 <jht@jusfoun.com>
2019-08-19 20:54:00 +0800

    update

---
+++ hello-scala/src/com/hello/scala/MainTest.scala
@@ -0,0 +1,12 @@
+package com.hello.scala
+
+object MainTest {
+
+	def main(args: Array[String]): Unit = {
+
+		// 输出 Hello World
+		// 这是一个单行注释
+		println("hello scala")
+	}
+
+}
---
+++ hello-spark2.3.1-scala2.11.12-maven/src/test/scala/com/hello/test/spark/scala/MainTest.scala
@@ -0,0 +1,9 @@
+package com.hello.test.spark.scala
+
+object MainTest {
+
+	def main(args: Array[String]): Unit = {
+		println("main test")
+	}
+
+}
