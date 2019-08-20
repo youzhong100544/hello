24e00a5518d251553a33febdfdd667bc1b25d715
姜海涛 <jht@jusfoun.com>
2019-08-20 20:59:40 +0800

    update

--- hello-scala-maven/src/test/scala/com.hello.test/scala/FastJsonUtilTest.scala
+++ hello-scala-maven/src/test/scala/com.hello.test/scala/FastJsonUtilTest.scala
@@ -4,33 +4,59 @@ package com.hello.test.scala
 import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
 import com.hello.scala.utils.FastJsonUtil
 import com.hello.scala.utils.FastJsonUtil.strToJSONObject
+import org.junit.Test
+
+import scala.collection.immutable
+import scala.io.Source
 
 object FastJsonUtilTest {
-	def main(args: Array[String]): Unit = {
 
-		/*
+	/*
 {
-	"dataId":123,
-	"dataType":"mysql",
-	"resultData":[
-				  {"binlog":"mysql_binlog.000","column":[{"name":"single","type":"int(5)"},{"name":"single3","type":"int(5)"}]},
-				  {"binlog1":"redis_binlog.000","column":[{"name":"single3","type":"int(5)"},{"name":"single3","type":"int(5)"}]},
-	]
+"dataId":123,
+"dataType":"mysql",
+"resultData":[
+				{"binlog":"mysql_binlog.000","column":[{"name":"single","type":"int(5)"},{"name":"single3","type":"int(5)"}]},
+				{"binlog1":"redis_binlog.000","column":[{"name":"single3","type":"int(5)"},{"name":"single3","type":"int(5)"}]},
+]
 }
-		* */
-		val str0: String = "{\n\t\"dataId\":123,\n\t\"dataType\":\"mysql\",\n\t\"resultData\":[\n\t\t\t\t  {\"binlog\":\"mysql_binlog.000\",\"column\":[{\"name\":\"single\",\"type\":\"int(5)\"},{\"name\":\"single3\",\"type\":\"int(5)\"}]},\n\t\t\t\t  {\"binlog1\":\"redis_binlog.000\",\"column\":[{\"name\":\"single3\",\"type\":\"int(5)\"},{\"name\":\"single3\",\"type\":\"int(5)\"}]},\n\t\t\t     ]\n}"
+	* */
+	val str1: String = "{\n\t\"dataId\":123,\n\t\"dataType\":\"mysql\",\n\t\"resultData\":[\n\t\t\t\t  {\"binlog\":\"mysql_binlog.000\",\"column\":[{\"name\":\"single\",\"type\":\"int(5)\"},{\"name\":\"single3\",\"type\":\"int(5)\"}]},\n\t\t\t\t  {\"binlog1\":\"redis_binlog.000\",\"column\":[{\"name\":\"single3\",\"type\":\"int(5)\"},{\"name\":\"single3\",\"type\":\"int(5)\"}]},\n\t\t\t     ]\n}"
 
 
-		val str1: String = "{\"name\":\"张三\",\"age\":10}"
+	val str2: String = "{\"name\":\"张三\",\"age\":10}"
 
-		val str2: String = "{\"data\":[{\"label\":\"123\",\"acc\":1,\"version\":\"4.3.1\"}]}"
+	val str3: String = "{\"data\":[{\"label\":\"123\",\"acc\":1,\"version\":\"4.3.1\"}]}"
 
-		val str3: String = "{\"data\":[{\"label\":\"789\",\"acc\":1,\"version\":\"4.3.1\"},{\"label\":\"78\",\"acc\":100,\"version\":\"4.3.1\"}]}"
+	val str4: String = "{\"data\":[{\"label\":\"789\",\"acc\":1,\"version\":\"4.3.1\"},{\"label\":\"78\",\"acc\":100,\"version\":\"4.3.1\"}]}"
 
 
+	def main(args: Array[String]): Unit = {
+		val test = new FastJsonUtilTest
+
+		test.test01()
+		test.test02()
+		test.test03()
+		test.test04()
+
+
+		println("test05------------")
+		println()
+		test.test05()
+
+
+
+
+
+	}
+}
+
+class FastJsonUtilTest{
+
+	def test01(): Unit = {
 		/* ----------------------------------------- */
 		// 对于JSON中的套JSON字符串的可以使用
-		val jsonObject: JSONObject = JSON.parseObject(str0)
+		val jsonObject: JSONObject = JSON.parseObject(FastJsonUtilTest.str1)
 
 		val resultDataStr : String = jsonObject.getString("resultData")
 		println("resultDataStr------------")
@@ -76,30 +102,75 @@ object FastJsonUtilTest {
 		println("-----------------------------------------------------------------------------------")
 
 
+	}
 
-
-
+	def test02(): Unit = {
 		/* ------------------------------------------------------------------------- */
 
-		val nObject: JSONObject = strToJSONObject(str1)
+		val nObject: JSONObject = strToJSONObject(FastJsonUtilTest.str2)
 		println("name: " + nObject.get("name"))
 		println("name: " + nObject.getString("name"))
 		println("age: " + nObject.getInteger("age"))
 
 		/* ----------------------------------------- */
+	}
 
-		val jObject: JSONObject = FastJsonUtil.strToJSONObject(str2)
+	def test03(): Unit = {
+
+
+		val jObject: JSONObject = FastJsonUtil.strToJSONObject(FastJsonUtilTest.str3)
 		println("data: " + jObject.get("data"))
+	}
+
+	def test04(): Unit = {
+
 
 		/* ----------------------------------------- */
 		val fj = new FastJsonUtil
-		val sObject: JSONObject = fj.strToJSONObject(str3)
+		val sObject: JSONObject = fj.strToJSONObject(FastJsonUtilTest.str4)
 		val data: AnyRef = sObject.get("data")
 		println("data: " + data)
 
-
-
 		/* ----------------------------------------- */
+	}
+
+	def test05(): Unit = {
+		val lines: Iterator[String] = Source.fromFile("json.txt").getLines()
+		val list: List[String] = lines.toList
+		val jSONObjects: immutable.Seq[JSONObject] = list.map(x => {  //取出每一条数据，把数据转换成JSONObject类型
+			println(x)
+			JSON.parseObject(x)
+		})
+		jSONObjects.foreach( t =>{
+			val str: String = t.getString("resultData")  //取出resultData的数据，
+			val oNArray: JSONArray = t.getJSONArray("resultData")
+			//result对应的数据是一个 数组中 存的是 [{json字符串},{json字符串}]
+
+			// /想要遍历JSONArray中的数据可以使用
+			import scala.collection.JavaConversions._  //可以把Java中的集合转成Scala中的集合
+			val list: List[AnyRef] = oNArray.iterator().toList
+			val listOBJ: List[JSONObject] = list.map(m=> JSON.parseObject(m.toString)/*或者使用m.asInstanceOf[JSONObject]*/)
+
+			val str1 = oNArray.getString(0)
+			//也可以通过getJSONObject(下标)  获取相应的JSONObject
+			val nObject: JSONObject = oNArray.getJSONObject(1)
+			//获取column
+			val value = nObject.getString("column")
+			val array = nObject.getJSONArray("column")
+			println(str1)
+			println(value)
+			println(array.getString(0))
+			val on1 = array.getJSONObject(0)
+			val str3 = on1.getString("modified")
+			println(str3)
+
+		})
+
+	}
+
+
+	@Test
+	def test06(): Unit = {
 
 	}
 }
