2b10e730fe27fbaeacaeed9a9f631e1ac74d7429
calm <youzhong100544@aliyun.com>
2020-04-03 14:44:03 +0800

    优化

---
+++ hello-spark_2.3.0-java_1.8-maven/src/main/java/com/hello/spark/java/entity/Iris.java
@@ -0,0 +1,19 @@
+package com.hello.spark.java.entity;
+
+public class Iris {
+
+	private float SepalLengthCm;
+	private float SepalWidthCm;
+	private float PetalLengthCm;
+	private float PetalWidthCm;
+
+	private String Species;
+
+
+
+
+
+
+
+
+}
