bf94fba4ceb7b54221191de13ac46826d5cf448c
姜海涛 <jht@jusfoun.com>
2020-04-15 17:53:58 +0800

    优化

--- hello-python_3.6/hello/matplotlib/hello-matplotlib.py
+++ hello-python_3.6/hello/matplotlib/hello-matplotlib.py
@@ -3,8 +3,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib as mp
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
+
+import matplotlib.pylab as plb
+# from matplotlib import pylab as plb
 
 print("numpy version: {}".format(np.__version__))
 print("pandas version: {}".format(pd.__version__))
