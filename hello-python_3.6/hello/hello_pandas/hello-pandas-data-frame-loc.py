0b1c7f9d5cb147ed44b0325ebd06c905c133c64f
姜海涛 <jht@jusfoun.com>
2020-04-21 17:45:19 +0800

    commit and push

--- hello-python_3.6/hello/hello_pandas/hello-pandas-data-frame-loc.py
+++ hello-python_3.6/hello/hello_pandas/hello-pandas-data-frame-loc.py
@@ -12,38 +12,11 @@ from hello.hello_pandas.common import common
 print("pandas version: {}".format(pd.__version__))
 print()
 
-# True就是可以换行显示。设置成False的时候不允许换行
 pd.set_option('expand_frame_repr', False)
-
-# 显示的最大行数和列数，如果超额就显示省略号，这个指的是多少个dataFrame的列。如果比较多又不允许换行，就会显得很乱。
-# pd.set_option('display.max_rows', 10)
-# pd.set_option('display.max_columns', 10)
-# 显示所有列
 pd.set_option('display.max_columns', None)
-# 显示所有行
 pd.set_option('display.max_rows', None)
-
-# 显示小数点后的位数
-# pd.set_option('precision', 5)
-
-# truncate表示截断，info表示查看信息，一般选truncate
-# pd.set_option('large_repr', "info")
-
-# 列长度
-# 设置 value 的显示长度为100，默认为50
 pd.set_option('max_colwidth', 10000)
-
-# 绝对值小于0.5的显示0.0
-# pd.set_option('chop_threshold', 0.5)
-
-# 显示居中还是左边，
-# pd.set_option('colheader_justify', 'left')
-
-# 横向最多显示多少个字符， 一般80不适合横向的屏幕，平时多用200.
 pd.set_option('display.width', 1024)
-
-# 输出时列名对齐列数据
-# 这两个参数的默认设置都是False
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
 
@@ -58,7 +31,6 @@ df_a = pd.DataFrame(ls_a)
 df1 = pd.DataFrame(data=[[11, 12, 13], [14, 15, 16], [17, 18, 19]], index=[11, 22, 33], columns=['aa', 'bb', 'cc'])
 
 df2 = pd.DataFrame(data=[[10, 20, 30], [40, 50, 60], [70, 80, 90]], index=['e', 'f', 'g'], columns=['aa', 'bb', 'cc'])
-
 
 
 print()
@@ -98,6 +70,7 @@ Name: 0, dtype: int64
 print("-"*100)
 
 # loc 索引行，label 是整型数字
+# 如果对 df1 这么写：df1.loc[0]会报错，因为loc索引的是label，显然在df2的行的名字中没有叫0的。
 print(df1.loc[11])
 '''
 aa    11
@@ -139,3 +112,4 @@ print(df_a.loc[1:])
 '''
 
 print("-"*100)
+
