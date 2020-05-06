61d339eb81fc6631e6b13eb1e68eb55c20bb9e4a
hiahia <youzhong100544@aliyun.com>
2020-05-06 15:40:05 +0800

    commit

--- hello-python_3.6/hello/hello_matplotlib/hello-matplotlib.py
+++ hello-python_3.6/hello/hello_matplotlib/hello-matplotlib.py
@@ -18,16 +18,16 @@ print("matplotlib version: {}".format(mp.__version__))
 ls_a = {"A": [1, 1, 2, 2, 2, 3], "B": [2, 5, 8, 7, 9, 4], "C": [3, 6, 9, 9, 9, 9]}
 df = pd.DataFrame(ls_a)
 
-fig,ax=plt.subplots()
+fig, ax = plt.subplots()
 # Gaussian=np.random.normal(0,1,1000) #创建一组平均数为0，标准差为1，总个数为1000的符合标准正态分布的数据
-ax.hist(df["A"],bins=25,histtype="stepfilled",normed=True,alpha=0.6)
-sns.kdeplot(df["A"],shade=True)
+ax.hist(df["A"], bins=25, histtype="stepfilled", normed=True, alpha=0.6)
+sns.kdeplot(df["A"], shade=True)
 plt.show()
 
-fig, ax=plt.subplots()
+fig, ax = plt.subplots()
 for dsf in df.columns:
-    ax.hist(df[dsf],bins=25,histtype="stepfilled",normed=True,alpha=0.6)
-    sns.kdeplot(df[dsf],shade=True)
+    ax.hist(df[dsf], bins=25, histtype="stepfilled", normed=True, alpha=0.6)
+    sns.kdeplot(df[dsf], shade=True)
     # plt.show()
 
 plt.show()
