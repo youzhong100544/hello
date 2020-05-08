80e99a25a346240c8561698697d8043550b892d9
calm <youzhong100544@aliyun.com>
2020-05-08 23:02:23 +0800

    优化

---
+++ hello-python_3.6/hello/hello_sklearn/hello-sklearn-TF-IDF.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import sklearn
+
+from sklearn import datasets
+from sklearn.datasets import load_digits
+
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import learning_curve, ShuffleSplit
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
